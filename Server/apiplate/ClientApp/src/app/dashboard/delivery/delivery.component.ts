731819c8907cf12db69d8c72331d093e6bb7e554
almunzir99 <almunzir99@gmail.com>
2022-01-11 23:55:42 +0200

    design and integrate delivery module

--- Server/apiplate/ClientApp/src/app/dashboard/delivery/delivery.component.ts
+++ Server/apiplate/ClientApp/src/app/dashboard/delivery/delivery.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FuiModalService } from 'ngx-fomantic-ui';
+import { Delivery } from 'src/app/core/models/delivery.model';
+import { DeliveryService } from 'src/app/core/services/delivery.service';
+import { Column } from 'src/app/shared/data-table/models/column.model';
+import { ControlTypes } from 'src/app/shared/form-builder/models/control-type.enum';
+import { FormBuilderGroup } from 'src/app/shared/form-builder/models/form-builder-group.model';
+import { FormBuilderModal } from 'src/app/shared/modals/form-builder-modal/form-builder-modal.component';
+import { MessageModal, MessageTypes } from 'src/app/shared/modals/message-modal/message-modal.component';
 
 @Component({
   selector: 'app-delivery',
@@ -7,9 +16,262 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cols: Column[];
+  drivers: Delivery[] = [];
+  isDataLoading = false;
+  pageIndex = 1;
+  pageSize = 10;
+  totalRecords = 0;
+  totalPages = 1;
+  DimLoading = false;
+  orderBy = "lastUpdate";
+  ascending = false;
+  searchValue = "";
+  constructor(private _service: DeliveryService,private modalService: FuiModalService) {
+   
   }
+  onSearchChange(value){
+    this.searchValue = value;
+    this.initData();
+  }
+  onSortChange(event){
+    this.orderBy = event.orderBy;
+    this.ascending = event.ascending;
+    this.initData();
+
+  }
+   
+  initData() {
+    this.isDataLoading = true;
+    this._service.get(this.pageIndex, this.pageSize,this.searchValue,this.orderBy,this.ascending).subscribe(res => {
+      this.drivers = res.data;
+      this.totalRecords = res.totalRecords;
+      this.totalPages = res.totalPages;
+      this.isDataLoading = false;
+
+    }, err => {
+      this.isDataLoading = false;
+
+    });
+  }
+  initCols() {
+    this.cols = [
+             {
+        prop: "id",
+        title: "الرقم التعريفي",
+        show:true,
+        sortable:true
+
+      },
+     
+      {
+        prop: "username",
+        title: "اسم المستخدم",
+        show:true,
+        sortable:true
+
+      },
+      {
+        prop: "phone",
+        title: "رقم الهاتف",
+        show:true,
+        sortable:true
+
+
+      },
+       
+      {
+        prop: "createdAt",
+        title: "تاريخ الانشاء",
+        show:true,
+        sortable:true
+
+
+      },
+      {
+        prop: "lastUpdate",
+        title: "تاريخ التعديل",
+        show:true,
+        sortable:true
+
+
+      }, {
+        prop: "Actions",
+        title: "الافعال",
+        show:true,
+
+      }
+    ]
+  }
+  ngOnInit(): void {
+    this.initCols();
+    this.initData();
+  }
+  pageIndexChanged(index: number) {
+    this.pageIndex = index;
+    this.initData();
+
+  }
+  pageSizeChanged(size: number) {
+    this.pageSize = size;
+    this.initData();
+
+  }
+  delete(id:number) {
+    this.modalService
+      .open(new MessageModal({
+        title: "تأكيد",
+        content: "هل انت متأكد كن انك تريد حذف هذا العنصر ؟", isConfirm: true , messageType:MessageTypes.Danger
+      }))
+      .onApprove(() => {
+          this.DimLoading = true;
+          this._service.delete(id).subscribe(res =>{
+              this.DimLoading = false;
+              this.modalService.open(new MessageModal({
+                title: "نجاح",
+                content: "تم حذف العنصر بنجاح", isConfirm: false , messageType:MessageTypes.Success
+              }))
+              this.initData();
+          },err =>{
+            this.DimLoading = false;
+
+          });
+      })
+      
+  }
+  openFormModal(driver:Delivery) {
+      const form: FormBuilderGroup[] = [
+        {
+
+          title: "عام",
+          controls: [
+            {
+              title: "id",
+              name: "id",
+              controlType: ControlTypes.Hidden,
+              width: "0px",
+              value: driver ? driver.id : undefined
+            },
+            {
+              title: "اسم المستخدم",
+              name: "username",
+              icon: "user tie icon",
+              controlType: ControlTypes.TextInput,
+              width: "50%",
+              value: driver ? driver.username : undefined,
+              validators: [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(25),
+              ]
+            },
+            {
+              title: "رقم الهاتف",
+              name: "phone",
+              icon: "phone icon",
+              controlType: ControlTypes.NumberInput,
+              width: "50%",
+              value: driver ? driver.phone : undefined,
+              validators:[
+                Validators.required,
+                Validators.minLength(10),
+                Validators.maxLength(12),
+
+              ]
+
+            },
+             
+            {
+              title: "كلمة المرور",
+              name: "password",
+              icon: "key icon",
+
+              controlType: ControlTypes.PasswordInput,
+              width: "50%",
+              validators:[
+                Validators.required
+              ]
+
+            },
+            {
+              title: "اعد ادخال كلمة المرور",
+              name: "repassword",
+              icon: "key icon",
+              validators:[
+                Validators.required
+              ],
+
+              controlType: ControlTypes.PasswordInput,
+              width: "50%"
+            },
+            {
+              title: "الصورة",
+              name: "image",
+              controlType: ControlTypes.LocalFilePicker,
+              width: "100%",
+              value: driver ? driver.image : undefined,
+
+            },
+            
+            
+          ]
+        }
+      ];
+      this.modalService.open(new FormBuilderModal(
+        {
+          title: driver ? "تعديل بيانات مستخدم" : "مستخدم جديد",
+          controlGroups: form
+        }
+      )).onApprove((result) => {
+        if(driver)
+        this.update(result as Delivery); 
+        else 
+        this.create(result as Delivery); 
+      
+      });
+     }
+  create(driver:Delivery){
+    this.DimLoading = true;
+    this._service.post(driver).subscribe(res =>{
+      this.initData();
+      this.DimLoading = false;
+      this.modalService.open(new MessageModal({
+        title: "نجاح",
+        content: "تم انشاء العنصر بنجاح", isConfirm: false , messageType:MessageTypes.Success
+      }))
+    },err=>{
+      console.log(err);
+      this.DimLoading = false;
+       
+    })
+  }
+  update(delivery:Delivery){
+    this.DimLoading = true;
+    this._service.put(delivery).subscribe(res =>{
+      this.initData();
+      this.DimLoading = false;
+      this.modalService.open(new MessageModal({
+        title: "نجاج",
+        content: "تم تحديث بيانات العنصر بنجاح", isConfirm: false , messageType:MessageTypes.Success
+      }))
+    },err=>{
+      console.log(err);
+      this.DimLoading = false;
+       
+    })
+  }
+  
+  range(total: number, beginIndex: number = 0, step: number = 1): number[] {
+    let list: number[] = [];
+    for (let index = beginIndex; index < total; index += step) {
+      list.push(index);
+    }
+    return list;
+  }
+  downloadCSV(){
+    this._service.downloadCSV();
+  }
+
+
 
 }
--- Server/apiplate/ClientApp/src/app/dashboard/delivery/delivery.module.ts
+++ Server/apiplate/ClientApp/src/app/dashboard/delivery/delivery.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DeliveryRoutingModule } from './delivery-routing.module';
 import { DeliveryComponent } from './delivery.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { FomanticUIModule } from 'ngx-fomantic-ui';
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { DeliveryComponent } from './delivery.component';
   ],
   imports: [
     CommonModule,
-    DeliveryRoutingModule
+    DeliveryRoutingModule,
+    SharedModule,
+    FormsModule,
+    FomanticUIModule
   ]
 })
 export class DeliveryModule { }
