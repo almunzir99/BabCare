820f27eb742730abe9e3f2a17c8f2371746d8669
almunzir99 <almunzir99@gmail.com>
2022-04-22 23:38:37 +0200

    add chart design to dashboard page

--- Server/apiplate/ClientApp/src/app/dashboard/home/home.component.ts
+++ Server/apiplate/ClientApp/src/app/dashboard/home/home.component.ts
@@ -10,6 +10,70 @@ import { StatisticsService } from 'src/app/core/services/statistics.service';
 export class HomeComponent implements OnInit {
   isLoading  = false;
   stats:Statistics;
+  categoriesSampleData = [
+    {
+      name:"بتيزا",
+      value:200,
+    },
+    {
+      name:"بيرقر",
+      value:500,
+    },
+    {
+      name:"عصائر ",
+      value:1000,
+    },
+    {
+      name:"بلدي",
+      value:350,
+    }
+  ];
+  ordersSampleData = [
+    {
+      name :"طلبات خلال الاسبوع",
+      series:[
+        {
+          name:"السبت",
+          value: 200,
+
+        },
+        {
+          name:"الاحد",
+          value: 100,
+
+        },
+        {
+          name:"الاثنين",
+          value: 150,
+
+        },
+        {
+          name:"الثلاثاء",
+          value: 80,
+
+        },
+        {
+          name:"الاربعاء",
+          value: 250,
+
+        },
+        {
+          name:"الخميس",
+          value: 210,
+
+        },
+        {
+          name:"الجمعة",
+          value: 160,
+
+        }
+      ]
+    }
+  ];
+  colorScheme = {
+    domain: ["#DC143C","#DC143C90","#DC143C80","#DC143C70","#DC143C60"]
+  };
+
   constructor(private _service:StatisticsService) {
     this.getStats();
   }
@@ -22,6 +86,7 @@ export class HomeComponent implements OnInit {
         this.isLoading = false;
     });
   }
+
   ngOnInit(): void {
   }
 
